00850903186f0833f6a012928a9e753147785311
ddoniddoni <psdkei@naver.com>
2020-04-04 01:46:45 +0900

    Setting Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
     return count + 1;
