7aee168559a87e68b13be341c527482bca9bd97b
ddoniddoni <psdkei@naver.com>
2020-04-04 01:35:33 +0900

    Setting Redux

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "minus" });
+
+console.log(countStore.getState());
